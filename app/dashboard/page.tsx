625827d3f37952a9362932938a8f2ece451db691
jinlee0310 <onenmore.jin@gmail.com>
2024-12-06 14:26:41 +0900

    feat: Button 컴포넌트 테스트

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 export default function Page() {
-  return <div>lint-staged test</div>;
+  return (
+    <div>
+      <Button size={"sm"} className={"bg-red-500 text-white"}>
+        공유하기
+      </Button>
+      <Button size={"default"} className={"bg-red-500 text-white"}>
+        공유하기
+      </Button>
+      <Button size={"lg"} className={"bg-red-500 text-white"}>
+        공유하기
+      </Button>
+    </div>
+  );
 }
