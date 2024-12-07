9d7fd2f13679695ac321a62458e766eedc543b31
jinlee0310 <onenmore.jin@gmail.com>
2024-12-07 17:15:55 +0900

    feat: sentry 초기설정을 추가합니다

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
+import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// Make sure adding Sentry options is the last code to run before exporting
+export default withSentryConfig(nextConfig, {
+  org: "ttogeul",
+  project: "frontend",
+
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  hideSourceMaps: true,
+
+  silent: false, // Can be used to suppress logs
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
