47821e78f0e391c896d297793b22525b6d51d510
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2024-12-09 17:21:03 +0900

    feat : 메인 페이지 ui 우선 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>clean up</main>;
+  return (
+    <main>
+      <div>2024</div>
+      <h1 className="flex flex-col">
+        <span>
+          내 <span className="text-red-500">블로그</span>
+        </span>
+        <span>돌아보기</span>
+      </h1>
+      <Button>분석하기</Button>
+    </main>
+  );
 }
