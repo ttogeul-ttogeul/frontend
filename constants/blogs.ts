2ea762073280da7de395e4ec556a1c4dfd6a9422
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2024-12-10 21:23:22 +0900

    feat : BLOGS에 homeUrl 추가

--- constants/blogs.ts
+++ constants/blogs.ts
@@ -1,23 +1,26 @@
 export const BLOGS = [
   {
     id: "naver",
-    title: "네이버 블로그",
+    name: "네이버 블로그",
     icon: "",
     bgColor: "bg-[#00C73C]",
     urlExample: "https://blog.naver.com/example",
+    homeUrl: "https://blog.naver.com",
   },
   {
     id: "tistory",
-    title: "티스토리",
+    name: "티스토리",
     icon: "",
     bgColor: "bg-[#EB531F]",
     urlExample: "https://example.tistory.com",
+    homeUrl: "https://www.tistory.com",
   },
   {
     id: "velog",
-    title: "Velog",
+    name: "Velog",
     icon: "",
     bgColor: "bg-[#20C997]",
     urlExample: "https://velog.io/@example",
+    homeUrl: "https://velog.io",
   },
 ];
