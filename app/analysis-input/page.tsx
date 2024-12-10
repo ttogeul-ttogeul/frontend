5022cadf00d9f751a8d74ef1b1eda4e135755447
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2024-12-10 22:00:53 +0900

    feat: 블로그 선택 버튼 로직 구현

--- app/analysis-input/page.tsx
+++ app/analysis-input/page.tsx
@@ -3,15 +3,24 @@
 import { useState } from "react";
 import { SelectBlogSection, InputBlogSection } from ".";
 import Title from "@/components/analysis-input/title";
+import { type BlogType } from "@/constants/blogs";
 
 export default function Page() {
-  const [selectMode] = useState(true);
+  const [selectMode, setSelectMode] = useState(true);
+  const [blogType, setBlogType] = useState<BlogType>("tistory");
 
   return (
     <div>
       <Title selectMode={selectMode} />
 
-      {selectMode ? <SelectBlogSection /> : <InputBlogSection />}
+      {selectMode ? (
+        <SelectBlogSection
+          setSelectMode={setSelectMode}
+          setBlogType={setBlogType}
+        />
+      ) : (
+        <InputBlogSection blogType={blogType} />
+      )}
     </div>
   );
 }
