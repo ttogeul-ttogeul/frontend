73c59a66a5d6fca93bbf88f2c468c9a2b575d1f7
Inae Choi <inae_choi2@tmax.co.kr>
2024-12-13 03:07:16 +0900

    feat: Input 컴포넌트 디자인 반영

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "flex w-full rounded-lg bg-gray-50 px-5 py-3 text-lg text-gray-950 caret-violet-600 transition-colors placeholder:text-gray-400 hover:ring-2 hover:ring-violet-500 focus:outline-none focus:ring-2 focus:ring-violet-500 disabled:cursor-not-allowed disabled:bg-gray-50/40",
           className,
         )}
         ref={ref}
