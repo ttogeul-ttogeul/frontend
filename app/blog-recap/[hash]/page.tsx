103a3cb7444b2b28d4864e450084ce65825b9d3b
jinlee0310 <onenmore.jin@gmail.com>
2024-12-13 03:42:13 +0900

    feat: blog-recap 페이지 구현

--- app/blog-recap/[hash]/page.tsx
+++ app/blog-recap/[hash]/page.tsx
@@ -1,3 +1,24 @@
+import Seperator from "@/components/ui/seperator";
+import Text from "@/components/ui/text";
+import PersonaSection from "./persona-section";
+import QuoteSection from "./quote-section";
+import StatSection from "./stat-section";
+import FooterSection from "./footer-section";
+
 export default function Page() {
-  return <div>blog recap</div>;
+  return (
+    <main className={"flex flex-col items-center px-6 py-8"}>
+      <Text as="h1" className={"text-3xl font-bold"}>
+        User Nickname
+      </Text>
+      <Seperator className={"h-20"} />
+      <PersonaSection />
+      <Seperator className={"h-20"} />
+      <QuoteSection />
+      <Seperator className={"h-20"} />
+      <StatSection />
+      <Seperator className={"h-20"} />
+      <FooterSection />
+    </main>
+  );
 }
