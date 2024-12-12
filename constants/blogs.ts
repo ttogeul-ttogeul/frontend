ee5d0aac9b89efac4b85ed1fc4b16598a9157b5a
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2024-12-12 23:57:53 +0900

    feat: 상수에서 네이버 삭제

--- constants/blogs.ts
+++ constants/blogs.ts
@@ -1,13 +1,5 @@
 export const BLOGS = [
   {
-    id: "naver",
-    name: "네이버 블로그",
-    icon: "",
-    bgColor: "bg-blog-naver",
-    urlExample: "https://blog.naver.com/example",
-    homeUrl: "https://blog.naver.com",
-  },
-  {
     id: "tistory",
     name: "티스토리",
     icon: "",
