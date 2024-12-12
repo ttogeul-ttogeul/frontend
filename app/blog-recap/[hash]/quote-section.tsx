baaeb60ca31bee22d9f2a71afa187d167fdbac39
jinlee0310 <onenmore.jin@gmail.com>
2024-12-13 03:41:31 +0900

    feat: 명언 section 추가

---
+++ app/blog-recap/[hash]/quote-section.tsx
@@ -0,0 +1,9 @@
+import QuoteCarousel from "@/components/blog-recap/quote-carousel";
+
+export default function QuoteSection() {
+  return (
+    <section>
+      <QuoteCarousel />
+    </section>
+  );
+}
