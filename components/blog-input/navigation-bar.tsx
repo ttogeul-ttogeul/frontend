c2091192882dc6e2a2a1f9c298203447881d3fc0
Inae Choi <inae_choi2@tmax.co.kr>
2024-12-13 11:48:45 +0900

    refactor: layout 제거 후 구조 변경
    - layout없이 page에서 navigation bar 렌더링하도록 수정
    - 이로 인한 navigation bar props 추가

--- components/blog-input/navigation-bar.tsx
+++ components/blog-input/navigation-bar.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
 
-export default function NavigationBar() {
-  const router = useRouter();
+type NavigationBarProps = {
+  onClickHandler: () => void;
+};
 
+export default function NavigationBar({ onClickHandler }: NavigationBarProps) {
   return (
     <div className="mb-5 mt-6">
-      <Button variant="ghost" size={"md"} onClick={() => router.back()}>
+      <Button variant="ghost" size={"md"} onClick={onClickHandler}>
         {"< "}뒤로
       </Button>
     </div>
