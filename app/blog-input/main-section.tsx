c2aaf6ada448f8219b953c573c8f0a14b9667da1
Inae Choi <inae_choi2@tmax.co.kr>
2024-12-14 13:16:47 +0900

    design: 버튼내 아이콘 위치 수정

--- app/blog-input/main-section.tsx
+++ app/blog-input/main-section.tsx
@@ -55,13 +55,13 @@ export default function MainSection({
             <Button
               key={blog.id}
               variant={"filled"}
-              className={`${blog.bgColor} text-white`}
+              className={`${blog.bgColor} relative text-white`}
               onClick={() => {
                 setBlogDomain(blog.id);
                 setSelectMode(false);
               }}
             >
-              {logos[blog.id]}
+              <div className="absolute left-6">{logos[blog.id]}</div>
               {blog.name}
             </Button>
           ))}
