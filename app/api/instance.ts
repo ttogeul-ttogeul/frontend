919b1e7ed8be43e52e478b89c4d5380bbb594b51
jinlee0310 <onenmore.jin@gmail.com>
2024-12-14 11:44:14 +0900

    fix: axios 에러 무한호출 버그 수정

--- app/api/instance.ts
+++ app/api/instance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as Sentry from "@sentry/nextjs";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL, // 환경 변수에서 API URL을 가져옴
@@ -25,6 +26,18 @@ axiosInstance.interceptors.response.use(
     return response.data;
   },
   (error) => {
-    return Promise.reject(error.response || error.message);
+    if (axios.isAxiosError(error)) {
+      Sentry.captureException(`API error: ${error.message}`);
+      return Promise.reject({
+        status: error.response?.status,
+        message: error.response?.data?.message || error.message,
+        data: error.response?.data,
+      });
+    }
+    Sentry.captureException(`unknown error`);
+    return Promise.reject({
+      message: "알 수 없는 에러가 발생했습니다.",
+      error: error,
+    });
   },
 );
