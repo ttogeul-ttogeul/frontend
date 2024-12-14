6da0af6ead44c7ad2ae99973f04d01e67e5c1cfc
Inae Choi <inae_choi2@tmax.co.kr>
2024-12-14 12:26:47 +0900

    feat: 수정된 Title 컴포넌트 적용

--- app/blog-input/title-section.tsx
+++ app/blog-input/title-section.tsx
@@ -1,5 +1,5 @@
 import { BlogDomain } from "@/constants/blogs";
-import HighlightedTitle from "../../components/home/highlighted-title";
+import Title from "../../components/home/highlighted-title";
 
 type TitleSectionProps = {
   selectMode: boolean;
@@ -13,27 +13,20 @@ export default function TitleSection({
   return (
     <div className="text-2xl/[1.875rem] font-medium">
       {selectMode ? (
-        <h1 className="flex flex-col">
-          <span>분석할</span>
-          <HighlightedTitle
-            highlight="블로그 서비스"
-            className="text-2xl/[1.875rem] font-medium"
-          >
-            를
-          </HighlightedTitle>
-          <span>선택하세요.</span>
-        </h1>
+        <Title className="flex flex-col text-2xl/[1.875rem] font-medium">
+          <div>
+            <Title.Highlighted>내 블로그 서비스</Title.Highlighted>를
+          </div>
+          <div>선택하세요</div>
+        </Title>
       ) : (
         <div>
-          <h1 className="flex flex-col">
-            <HighlightedTitle
-              highlight="블로그 링크"
-              className="text-2xl/[1.875rem] font-medium"
-            >
-              를
-            </HighlightedTitle>
-            <span>입력해주세요.</span>
-          </h1>
+          <Title className="flex flex-col text-2xl/[1.875rem] font-medium">
+            <div>
+              <Title.Highlighted>내 블로그 링크</Title.Highlighted>를
+            </div>
+            <div>입력하세요</div>
+          </Title>
           {blogDomain === "tistory" && (
             <p className="mt-3 text-base font-normal text-gray-400">
               HTML과 CSS로 스킨을 편집해서 사용하시는 경우에는 분석이 제한될 수
