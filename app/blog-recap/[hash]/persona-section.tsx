3d4a5d4b7e55fadc51a60dbd1080f2dc2a756235
jinlee0310 <onenmore.jin@gmail.com>
2024-12-15 01:06:28 +0900

    feat: persona section 데이터 연결

--- app/blog-recap/[hash]/persona-section.tsx
+++ app/blog-recap/[hash]/persona-section.tsx
@@ -1,21 +1,37 @@
+import { personaAtom } from "@/components/blog-recap/store/atom";
 import Seperator from "@/components/ui/seperator";
 import Text from "@/components/ui/text";
+import { useAtom } from "jotai";
 import Image from "next/image";
 
 export default function PersonaSection() {
+  const [persona] = useAtom(personaAtom);
   return (
-    <section className={"flex flex-col items-center"}>
+    <section className={"flex w-full flex-col items-center"}>
       <Text as="h2" className={"text-2xl font-bold"}>
-        이 블로거의 올해 성향
+        2024년, 이 블로그는
       </Text>
       <Seperator className="h-4" />
-      <div className={"border border-solid"}>
+      <div
+        className={
+          "flex flex-col items-center rounded-lg border border-solid border-gray-700 p-2 font-bold"
+        }
+      >
+        <div className={"mb-2 flex h-12 w-full items-center justify-center"}>
+          <Text as="h3" className={"text-2xl"}>
+            {persona.title}
+          </Text>
+        </div>
         <Image
-          src={"/images/sample-og.jpg"}
+          priority
+          src={persona.imageUrl}
           alt="persona"
           width={312}
           height={0}
         />
+        <div className={"mt-2 h-12 w-full"}>
+          <Text>{`${persona.category} 글을 가장 많이 작성하셨어요`}</Text>
+        </div>
       </div>
     </section>
   );
