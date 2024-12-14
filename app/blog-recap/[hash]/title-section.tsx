debb76c8064609b2133c5153a35aff13c148fe5b
jinlee0310 <onenmore.jin@gmail.com>
2024-12-15 00:34:12 +0900

    feat: blogTitle 데이터 연결

--- app/blog-recap/[hash]/title-section.tsx
+++ app/blog-recap/[hash]/title-section.tsx
@@ -1,9 +1,12 @@
+import { blogTitleAtom } from "@/components/blog-recap/store/atom";
 import Text from "@/components/ui/text";
+import { useAtom } from "jotai";
 
 export default function TitleSection() {
+  const [blogTitle] = useAtom(blogTitleAtom);
   return (
     <Text className={"text-3xl font-bold"} as="h1">
-      title
+      {blogTitle}
     </Text>
   );
 }
