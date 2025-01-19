a6b92869c4b59313fbf97a4b4a0d2d62941237f0
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2025-01-20 03:21:46 +0900

    rename: 파일명 수정으로 인한 변경

--- components/blog-input/index.ts
+++ components/blog-input/index.ts
@@ -2,4 +2,4 @@ export { default as TitleSection } from "./title-section";
 export { default as FooterSection } from "./footer-section";
 export { default as LoadingText } from "./loading-text";
 export { default as NavigationBar } from "./navigation-bar";
-export { default as AnalysisLoading } from "./analysis-loading";
+export { default as AnalysisLoading } from "./analysis-loading-lottie";
