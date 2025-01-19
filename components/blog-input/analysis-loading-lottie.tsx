2adb756f3d59c57beed1771ab24ebece2507c64d
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2025-01-20 03:21:07 +0900

    feat: Image에서 Lottie 컴포넌트 사용으로 변경

--- components/blog-input/analysis-loading-lottie.tsx
+++ components/blog-input/analysis-loading-lottie.tsx
@@ -1,20 +1,19 @@
+import dynamic from "next/dynamic";
+import spinnerLottie from "@/public/lotties/spinner.json";
 import LoadingText from "./loading-text";
-import Image from "next/image";
-import spinnerGif from "@/public/images/spinner.gif";
 
 export default function AnalysisLoading() {
+  const Lottie = dynamic(() => import("../shared/lottie"), { ssr: false });
+
   return (
     <div className="!z-100 absolute inset-0 h-dvh w-full">
       <div className="z-110 absolute left-1/2 mx-auto h-dvh w-full max-w-440pxr -translate-x-1/2 transform bg-gray-950">
         <div className="whitespace-pre text-center font-normal">
           <div className="mt-208pxr flex flex-col justify-center gap-8 align-middle">
             <div className="flex justify-center">
-              <Image
-                src={spinnerGif}
-                alt="Loading..."
-                width={128}
-                height={128}
-              />
+              <div className="h-32 w-32">
+                <Lottie animationData={spinnerLottie} />
+              </div>
             </div>
             <LoadingText />
           </div>
