d4bf2557d3d8fe4d9464fcea372d788bad5a2a55
jinlee0310 <onenmore.jin@gmail.com>
2025-01-27 12:35:15 +0900

    refactor: 타임아웃 삭제

--- app/api/instance.ts
+++ app/api/instance.ts
@@ -4,7 +4,6 @@ import { notFound } from "next/navigation";
 
 export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL, // 환경 변수에서 API URL을 가져옴
-  // timeout: 5000, // 요청 제한 시간 설정
   headers: {
     "Content-Type": "application/json",
   },
